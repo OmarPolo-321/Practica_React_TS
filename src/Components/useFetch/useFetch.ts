38b2f9a17d797d001ceb26a3c544ee4a6dbab1c1
Omar <omarpolo.bo@gmail.com>
2025-06-27 21:52:48 -0400

    Add_useFetch

--- src/Components/useFetch/useFetch.ts
+++ src/Components/useFetch/useFetch.ts
@@ -48,3 +48,29 @@ export const useFetch = <T>(url: string): Params<T> => {
 
   return { data, loading, error }
 }
+/* Como Utilizar lo
+import { useFetch } from './hooks/useFetch';
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+export const Posts = () => {
+  const { data, loading, error } = useFetch<Post[]>("https://jsonplaceholder.typicode.com/posts");
+
+  if (loading) return <p>Cargando...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <ul>
+      {data?.map(post => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+
+*/
